d09a32ccfb00ac06306c9ee2ebb1f66f0e0f3785
Leo Beltran <leonardo.casallas.beltran@gmail.com>
2019-09-28 11:49:59 -0500

    Solucion Reto05 Leonardo Casallas

--- src/index.js
+++ src/index.js
@@ -1,38 +1,57 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+window.localStorage.clear();
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://rickandmortyapi.com/api/character/";
+var storage = window.localStorage;
 
 const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
-        return `
+	fetch(api)
+		.then(response => response.json())
+		.then(response => {
+			const characters = response.results;
+			const nextPage = response.info.next;
+
+			storage.setItem("next_fetch", nextPage);
+
+			let output = characters
+				.map(character => {
+					return `
       <article class="Card">
         <img src="${character.image}" />
         <h2>${character.name}<span>${character.species}</span></h2>
       </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+    `;
+				})
+				.join("");
+			let newItem = document.createElement("section");
+			newItem.classList.add("Items");
+			newItem.innerHTML = output;
+			$app.appendChild(newItem);
+		})
+		.catch(error => console.log(error));
+};
 
-const loadData = () => {
-  getData(API);
-}
+const loadData = async () => {
+	const apinext = storage.getItem("next_fetch");
+	const api = apinext !== null ? apinext : API;
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
-  }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+	if (api === "") {
+		$observe.innerHTML = "<h2> Ya no hay mas personajes...</h2>";
+		intersectionObserver.unobserve($observe);
+	} else {
+		await getData(api);
+	}
+};
+
+const intersectionObserver = new IntersectionObserver(
+	entries => {
+		if (entries[0].isIntersecting) {
+			loadData();
+		}
+	},
+	{
+		rootMargin: "0px 0px 100% 0px"
+	}
+);
 
 intersectionObserver.observe($observe);
